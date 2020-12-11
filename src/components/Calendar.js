3e8d292f8255a270f8da3ad9c6355c89d4245153
Matthias Pinnewaert <pinny@MacBook-Pro.local>
2020-12-11 11:44:01 +0100

    working calendar view

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -2,27 +2,27 @@ import React, { useState } from 'react'
 import MonthView from './MonthView'
 
 function Calendar() {
-    const getToday = () => {
-        return new Date()
-    }
-    // The calendar view
-    // views: year, month, week
-    const [view, setView] = useState('month')
-    // The calendar date data
-    const [activeYear, setActiveYear] = useState(getToday().getFullYear())
-    const [activeMonth, setActiveMonth] = useState(getToday().getMonth())
-    const [activeDate, setActiveDate] = useState(getToday().getDate())
-    const [activeDay, setActiveDay] = useState(getToday().getDay())
-    return (
-        <div>
-            <MonthView 
-                year={activeYear} 
-                month={activeMonth}
-                date={activeDate} 
-                day={activeDay}
-            />
-        </div>
-    )
+  const today = new Date()
+  // The calendar view
+  // views: year, month, week
+  const [view, setView] = useState('month')
+  // The calendar date data
+  const [activeYear, setActiveYear] = useState(today.getFullYear())
+  const [activeMonth, setActiveMonth] = useState(today.getMonth())
+  const [activeDate, setActiveDate] = useState(today.getDate())
+  const [activeDay, setActiveDay] = useState(today.getDay())
+  return (
+    <div>
+      <MonthView
+        year={activeYear}
+        month={activeMonth}
+        date={activeDate}
+        day={activeDay}
+        setYear={setActiveYear}
+        setMonth={setActiveMonth}
+      />
+    </div>
+  )
 }
 
 export default Calendar
--- src/components/MonthView.js
+++ src/components/MonthView.js
@@ -1,67 +1,123 @@
 import React from 'react'
 function MonthView(props) {
-    function daysInMonth (month, year) { 
-        return new Date(year, month+1, 0).getDate(); 
-    } 
-    const Days = ['Ma', 'Di', 'Wo', 'Do', 'Vr', 'Za', 'Zo']
-    const Months = ['Januari', 'Februari', 'Maart', 'April', 'Mei', 'Juni', 'Juli', 'Augustus', 'September', 'Oktober', 'November', 'December']
-    const generateMonthViewArray = (month, year) => {
-        const firstDay = new Date(year, month, 1).getDay()
-        const dayPreMonth = daysInMonth(month-1, year)
-        const daysThisMonth = daysInMonth(month, year)
-        console.log(daysThisMonth, month)
-        console.log(dayPreMonth, month-1)
-        const lastDay = new Date(year, month, daysThisMonth).getDay()
-        console.log(firstDay, lastDay)
+  const Days = ['Ma', 'Di', 'Wo', 'Do', 'Vr', 'Za', 'Zo']
+  const Months = [
+    'Januari',
+    'Februari',
+    'Maart',
+    'April',
+    'Mei',
+    'Juni',
+    'Juli',
+    'Augustus',
+    'September',
+    'Oktober',
+    'November',
+    'December',
+  ]
+  function daysInMonth(month, year) {
+    return new Date(year, month + 1, 0).getDate()
+  }
+  const generateMonthViewArray = (month, year) => {
+    let firstDay = new Date(year, month, 1).getDay()
+    if (firstDay === 0) firstDay = 7 // Sunday is 0 in js
+    const dayPreMonth = daysInMonth(month - 1, year)
+    const daysThisMonth = daysInMonth(month, year)
+    const lastDay = new Date(year, month, daysThisMonth).getDay()
+    const firstDayToRender = dayPreMonth - firstDay + 1
+    const array = []
 
-        const firstDayToRender = dayPreMonth - firstDay + 1
-        console.log(firstDayToRender)
-        const array = []
-        for (let index = 1; index < firstDay; index++) {
-            array.push(firstDayToRender + index)
-        }
-        for (let index = 1; index <= daysThisMonth; index++) {
-            array.push(index)
-        }
-        for (let index = 1; index <= 7 - lastDay; index++) {
-            array.push(index)
-        }
-
-        console.log(array)
-        return array
+    // Premonth numbers
+    for (let index = 1; index < firstDay; index++) {
+      array.push(firstDayToRender + index)
     }
-    const monthArray = generateMonthViewArray(props.month, props.year)
-    return (
-        <div className='month-view'>
-            <div className='year'> {props.year} </div>
-            <div className='month'> {Months[props.month]} </div>
-            <div className='days'> 
-                {
-                    Days.map(day => {
-                        return ( <div key={day}> {day} </div> )
-                    })
-                }
+    // Actual month
+    for (let index = 1; index <= daysThisMonth; index++) {
+      array.push(index)
+    }
+    // Next month
+    for (let index = 1; index <= 7 - lastDay; index++) {
+      array.push(index)
+    }
+    return array
+  }
+  const monthArray = generateMonthViewArray(props.month, props.year)
+  return (
+    <div className="month-view">
+      <div className="year">
+        <a
+          onClick={() => {
+            props.setYear(props.year - 1)
+          }}
+        >
+          {' '}
+          &#8249;{' '}
+        </a>
+        <span> {props.year} </span>
+        <a
+          onClick={() => {
+            props.setYear(props.year + 1)
+          }}
+        >
+          {' '}
+          &#8250;{' '}
+        </a>
+      </div>
+      <div className="month">
+        <a
+          onClick={() => {
+            let newMonth = props.month - 1
+            if (newMonth < 0) {
+              newMonth = 11
+              props.setYear(props.year - 1)
+            }
+            props.setMonth(newMonth)
+          }}
+        >
+          {' '}
+          &#8249;{' '}
+        </a>
+        <span> {Months[props.month]} </span>
+        <a
+          onClick={() => {
+            let newMonth = props.month + 1
+            if (newMonth >= 12) {
+              newMonth = 0
+              props.setYear(props.year + 1)
+            }
+            props.setMonth(newMonth)
+          }}
+        >
+          {' '}
+          &#8250;{' '}
+        </a>
+      </div>
+      <div className="days">
+        {Days.map((day) => {
+          return <div key={day}> {day} </div>
+        })}
+      </div>
+      <div className="dates">
+        {monthArray.map((date, index) => {
+          return (
+            <div
+              key={index}
+              className={
+                props.year === new Date().getFullYear() &&
+                props.month === new Date().getMonth() &&
+                date === new Date().getDate()
+                  ? 'today'
+                  : ''
+              }
+            >
+              {date}
             </div>
-            <div className='dates'> 
-                {
-                    monthArray.map((date, index) => {
-                        return ( 
-                            <div key={index}
-                                className={
-                                    (props.year === (new Date().getFullYear()) &&
-                                    props.month === (new Date().getMonth()) &&
-                                    date === (new Date().getDate()) ) ? 'today' : ''
-                                }
-                            > 
-                                {date} 
-                            </div> )
-                    })
-                }
-                <div> </div>
-            </div>
-
-        </div>
-    )
+          )
+        })}
+        <div> </div>
+      </div>
+    </div>
+  )
 }
 
 export default MonthView
